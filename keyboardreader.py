19f144611b73e135936ed789fda6fca06f14be75
worldintro <worldintro@mail.ru>
2018-11-04 17:47:13 +0300

    added vector

--- keyboardreader.py
+++ keyboardreader.py
@@ -2,7 +2,8 @@ import pynput
 from pynput import keyboard
 import time
 
-# Dinamic dictionary - save double click
+vector = []
+# Dinamic dictionary - safety double click
 time_on_press = {}
 # Key hold time (list)
 hold_time = []
@@ -12,41 +13,55 @@ last_time = 0
 count = 0
 def on_press(key):
     try:
-        # Check time here
-        # Global time_on_press
-        time_on_press[key] = time.clock()
-        print('key {0} pressed'.format(key.char))
+        global count
+        if (key != keyboard.Key.enter):
+            # Check time here
+            time_on_press[key] = time.clock()
+            #print('key {0} pressed'.format(key.char))
+        elif (count != 0) & (key == keyboard.Key.enter):
+            # Stop listener
+            return False
     except AttributeError:
         print('special key {0} pressed'.format(key))
 
 def on_release(key):
     global count, last_time
-    time_now = time.clock()
-    # Hold time (little delay)
-    hold_time.append(time_now - time_on_press[key])
-
-    # Calculate between_time (skip first click)
-    if(count > 0):
-        between_time.append(time_now - last_time - (hold_time[count-1] + hold_time[count]))
-        print('between time is', between_time[count - 1])
-
-    # Print it
-    print('{0} released'.format(key))
-    print('hold time is', hold_time[count])
-    print("---")
-
-    # last_time for calculate between_time
-    last_time = time.clock()
-    count += 1
-    if key == keyboard.Key.esc:
+    if (key != keyboard.Key.enter):
+        time_now = time.clock()
+        # Hold time (little delay)
+        hold_time.append(time_now - time_on_press[key])
+        # Calculate between_time (skip first click)
+        if(count > 0):
+            between_time.append(time_now - last_time - (hold_time[count-1] + hold_time[count]))
+            #print('between time is', between_time[count - 1])
+        #print('{0} released'.format(key))
+        #print('hold time is', hold_time[count])
+        # last_time for calculate between_time
+        last_time = time.clock()
+        count += 1
+    elif (count != 0) & (key == keyboard.Key.enter):
         # Stop listener
         return False
 
+def reset():
+    global count, last_time
+    time_on_press.clear()
+    hold_time.clear()
+    between_time.clear()
+    last_time = 0
+    count = 0
+
 def main():
-    h = 0
-    with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
-        listener.join()
-    print(hold_time)
-    print(between_time)
+    while True:
+        with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
+            print("Type your keyword: ")
+            keyword = input()
+            listener.join()
+        vector.append([hold_time, between_time])
+        print("Hold time: ", hold_time)
+        print("Time between: ", between_time)
+        print("Vector size: ", len(vector))
+        # Reset lists
+        reset()
 if __name__ == "__main__":
     main()
